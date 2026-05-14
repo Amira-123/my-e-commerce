bf163a1fcad94e10f2086ccaebd39a4c6e4108d3
C007034 <amelhawary@ejada.com>
2026-05-14 22:48:32 +0300

    update project to 18

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,35 @@
-import { CartsModule } from './carts/carts.module';
- import { ProductsModule } from './products/products.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
-import { HttpClientModule } from '@angular/common/http'
+import { CartsModule } from './carts/carts.module';
+import { ProductsModule } from './products/products.module';
 import { ProductService } from './products/services/product.service';
-
-import { CommonModule } from '@angular/common';
 
 
 
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-
-
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule,
-    CartsModule,
-    ProductsModule,
-    HttpClientModule ,
-    CommonModule,
-
-  ],
-  providers: [
-    ProductService
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        SharedModule,
+        CartsModule,
+        ProductsModule,
+        CommonModule
+    ],
+    providers: [
+        ProductService,
+        provideHttpClient(withInterceptorsFromDi())
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,39 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
-import { BrowserModule } from '@angular/platform-browser';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { SelectComponent } from './components/select/select.component';
 
 
 
 
-@NgModule({
-  declarations: [
-    HeaderComponent,
-    SpinnerComponent,
-    SelectComponent,
-
-  ],
-  imports: [
-    CommonModule,
-    RouterModule,
-    BrowserModule,
-    HttpClientModule,
-    FormsModule
-
-
-  ],
-  exports:[
-    HeaderComponent,
-    BrowserModule,
-    SpinnerComponent,
-    SelectComponent,
-    FormsModule,
-    RouterModule,
-  ]
-})
+@NgModule({ declarations: [
+        HeaderComponent,
+        SpinnerComponent,
+        SelectComponent,
+    ],
+    exports: [
+        HeaderComponent,
+        SpinnerComponent,
+        SelectComponent,
+        FormsModule,
+        RouterModule,
+    ], imports: [
+        CommonModule,
+        RouterModule,
+        FormsModule
+    ], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class SharedModule { }
